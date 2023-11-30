4cf475d17ddec05388cbc197544c7e25ea4dfaa2
joao vitor <githubconta2021@gmail.com>
2023-11-30 12:14:38 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xadrez.Tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p; 
+
+            p = new Posicao(3,4);
+
+            Console.WriteLine($"Informaçoes: {p}");
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+using System;
+using System;
+using System.Globalization;
+
+namespace Xadrez.Tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha; 
+            this.Coluna = coluna;
+        } 
+        public override string ToString()
+        {
+            return $" Linha: {this.Linha}, Coluna: {this.Coluna}";
+        }
+    }
+}
